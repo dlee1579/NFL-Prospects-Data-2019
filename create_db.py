28634dfd47277847208629a562bc3ed883360da6
DL <dlee1579@gmail.com>
2019-04-23 22:55:22 -0500

    added functionality to update all defensive player profiles

--- create_db.py
+++ create_db.py
@@ -3,6 +3,7 @@ from test_folder import *
 # import matplotlib.pyplot as plt
 # from numpy import *
 from scipy.stats import skew, skewtest
+import time
 
 
 def create_database_lb(category="TT"):
@@ -97,6 +98,7 @@ def create_SPARQ_DB():
 	print(df)
 	#df.to_csv("SPARQ_lb.csv")
 
+
 def get_player_tackles(player, year="2018"):
 	tackles_dict = {
 		"Tot": "TT",
@@ -117,12 +119,19 @@ def get_player_tackles(player, year="2018"):
 		stats_recent = [player_df.loc[x] for x in player_df.index.to_list() if year in x][0]
 
 		# Returns Total Tackles, Solo Tackles, Tackles for Loss in that order
-		return [stats_recent["Tot"], stats_recent["Solo"], stats_recent["Loss"]]
+		return [stats_recent["Tot"], stats_recent["Solo"], stats_recent["Loss"], stats_recent["Sk"], stats_recent["Int"], stats_recent["PD"], stats_recent["FR"], stats_recent["FF"]]
 		# for tackle_type in tackles_types:
 		# 	#df.set_value("Vosean Joseph", tackles_dict[tackle_type],df2[tackle_type])
 		# 	df.at[player, tackles_dict[tackle_type]] = stats_recent[tackle_type]
-	except:
+	except (ValueError, IndexError, KeyError) as e:
 		print("Invalid player name.")
+		pass
+
+
+def update_database_lb():
+	# Update Linebacker Database with tackle information from Sports-Reference.com
+	df = pd.read_excel("test_folder/LB prospects 2015-2019.xlsm", sheet_name="Agg Data")
+
 
 if __name__ == "__main__":
 	# create_database_lb(category="TFL")
@@ -133,11 +142,45 @@ if __name__ == "__main__":
 	# create_SPARQ_DB()
 
 
-	# df = pd.read_excel("test_folder/LB prospects 2015-2019.xlsm", sheet_name="Agg Data")
-# 	#
+	#Initialize DataFrame of Player database, set name as index
+	# df = pd.read_excel("test_folder/LB prospects 2015-2019.xlsm", sheet_name="Agg Data").set_index("Name")
+	df = pd.read_excel("test_folder/NFL Defensive Prospects 2019.xlsx", sheet_name="LB").set_index("Name")
+	# Sampling n number of players
+	#players_recent = df.loc[df["Year"] == 2019].index.to_list()
+	players_recent = df.index.to_list()
+	# print(get_player_tackles("Alex Figueroa"))
+	print("Process started.")
+
+	start = time.time()
+
+	for player in players_recent:
+		print(player)
+		try:
+			[TT, ST, TFL, SK, INT, PD, FR, FF] = get_player_tackles(player)
+			df.at[player, "TT"] = TT
+			df.at[player, "ST"] = ST
+			df.at[player, "TFL"] = TFL
+			df.at[player, "SK"] = SK
+			df.at[player, "INT"] = INT
+			df.at[player, "PD"] = PD
+			df.at[player, "FR"] = FR
+			df.at[player, "FF"] = FF
+
+
+		except (TypeError, IndexError) as e:
+			print("%s does not have a Sports-Reference profile page." % player)
+			pass
+
+	end = time.time()
+	time_elapsed = end - start
+	print("Time elapsed: %f" % (time_elapsed))
+
+	# df.to_excel("Aggregate LB DataBase 2015-2019.xlsx", sheet_name="Agg Data")
+	df.to_excel("NFL DEF 2019.xlsx", sheet_name="LB")
+	#players_recent = df.
 # 	# # Set the Name column as the index
 # 	# df.set_index("Name", inplace=True)
 # 	# print(df)
-	print(get_player_tackles("abcde"))
+	#print(get_player_tackles("Patrick Willis"))
 
 
